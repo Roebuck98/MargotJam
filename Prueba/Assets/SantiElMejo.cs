577b3dc2cb53218e04ba83802568e58b92e90238
iTiago98 <itiago98@gmail.com>
2021-11-26 12:55:08 +0100

    santi el mejor

---
+++ Prueba/Assets/SantiElMejo.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SantiElMejo : MonoBehaviour
+{
+    void Start()
+    {
+        Debug.Log("¡Santi el mejor!");
+    }
+}
